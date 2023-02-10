e3b23ae92819d1fa79e6fb53abb47013df2840e9
Zhenya <115710959+ZhenyaLysachenko@users.noreply.github.com>
2023-02-10 20:28:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// 1 let div = document.createElement('div');
+
+//  2 tagElement.insertAdjacentHTML(position, text);
+//   Параметр  position - визначає позицію елемента що додається, 
+//   щодо елемента, що викликав метод.
+// 
+//   "beforebegin": до відкриваючого тега.
+//   "afterbegin": після відкриваючого тега.
+//   "beforeend": перед закриваючим тегом .
+//   "afterend": після закриваючого тега.
+
+// 3   За допомогою методу remove()
+//     Наприклад:
+//     div.remove();  
+
+
+
+
 function showElementsList(arr, parentUl = document.body) {
 
     let ul = document.createElement('ul');
